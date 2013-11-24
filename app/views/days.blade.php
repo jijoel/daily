5db4bd44d6ed00b78af31c13273c0ebc31ebe75d
Joel (home) <jijoel@yahoo.com>
2013-11-23 23:47:10 -1000

    initial commit

---
+++ app/views/days.blade.php
@@ -0,0 +1,3 @@
+<dl>
+    <a href="/"><dt>Day 1</dt><dd>Homepage</dd></a>
+</dl>
---
+++ app/views/home.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Daily Practice</title>
+
+    <link href="/css/home.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="container">
+
+        <h1>Aloha!</h1>
+
+        <p>I'm Joel, and I'm very inspired by <a href="http://jenniferdewalt.com/">Jennifer Dewalt's</a> 180 web sites in 180 days project. I don't think I'm quite that ambitious--just a 100 day challenge. :-) This will be a playground for me to spend a little time each day experimenting with web tools and techniques (both client and server side).</p>
+
+        <hr>
+
+        @include('days')
+
+    </div><!-- .container -->
+</body>
+</html>
