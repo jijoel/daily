0674f8b9ed14317899af72476ea091f0393d8aa0
Joel (home) <jijoel@yahoo.com>
2013-11-27 08:50:41 -1000

    updated route to handle get request for login

--- app/routes.php
+++ app/routes.php
@@ -60,8 +60,8 @@ Route::filter('day005_auth', function()
 });
 Route::get('day005_login', array('as'=>'day005_login', 
     'uses'=>'Days\Day005\AuthController@getLogin'));
-Route::post('day005_login', array(
-    'uses'=>'Days\Day005\AuthController@postLogin'));
+Route::get('day005_google', array(
+    'uses'=>'Days\Day005\AuthController@loginWithGoogle'));
 Route::get('day005_logout', array('as'=>'day005_logout', 
     'uses'=>'Days\Day005\AuthController@getLogout'));
 Route::get('day005_dashboard', array('as'=>'day005_dashboard',
