46a400e706c2962218d633ee419583e378c8e22b
Joel (home) <jijoel@yahoo.com>
2013-11-27 08:54:20 -1000

    update login screen for new url

--- app/views/days/005/login.blade.php
+++ app/views/days/005/login.blade.php
@@ -4,7 +4,7 @@
     
     <p class="note">This is an OAuth login page. Log in via your Google+ account.</p>
 
-    <p><a href="https://accounts.google.com/o/oauth2/auth?state=%2Fprofile&amp;redirect_uri=http://daily.kumuwai.com:80/day005_login&amp;response_type=code&amp;client_id=329408182449.apps.googleusercontent.com&amp;approval_prompt=force&amp;scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile">Login via Google</a></p>
+    <p><a href="https://accounts.google.com/o/oauth2/auth?state=%2Fprofile&amp;redirect_uri=http://daily.kumuwai.com:80/day005_login_google&amp;response_type=code&amp;client_id=329408182449.apps.googleusercontent.com&amp;approval_prompt=force&amp;scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile">Login via Google</a></p>
 
 
 @stop
