c56d276a08647c0b95ce945b856d4c8baecd017d
Joel (home) <jijoel@yahoo.com>
2013-12-06 09:27:40 -1000

    use standard image file for multiple days, instead of day-valued file

--- app/views/days/012/index.blade.php
+++ app/views/days/012/index.blade.php
@@ -2,7 +2,7 @@
 
 <p class="note">This is a very simple test using <a href="http://deepliquid.com/content/Jcrop.html">jcrop</a>.</p>
 
-<img id="main" src="/img/day011.jpg">
+<img id="main" src="/img/day.jpg">
 
 {{ Form::open(array('url'=>URL::route('day012.store'))) }}
     x: <input type="text" name="x" id="x" value="" />
@@ -11,12 +11,12 @@
     h: <input type="text" name="h" id="h" value="" />
     dw: <input type="text" name="dw" id="dw" value="" />
     dh: <input type="text" name="dh" id="dh" value="" />
-    img: <input type="text" name="img" id="img" value="/img/day011.jpg" />
+    img: <input type="text" name="img" id="img" value="/img/day.jpg" />
     {{ Form::submit('Submit') }}
 </form>
 
 <div id="preview-pane">
-    <img id="preview" src="/img/day011.jpg">
+    <img id="preview" src="/img/day.jpg">
 </div>
 
 @stop
