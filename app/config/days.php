b7131e1c6d5246d39d3e54b71ad700ca69176ad6
Joel (home) <jijoel@yahoo.com>
2013-12-05 23:50:41 -1000

    update days and routes for new day

--- app/config/days.php
+++ app/config/days.php
@@ -2,17 +2,18 @@
 
 return array(
 
-    //  Day #   Title               path
-    array('11', 'Image Cropping',   'day011'),
-    array('10', 'Playing with CSS', 'day010'),
-    array('9', 'Repository Todo',   'day009'),
-    array('8', 'RESTful Todos',     'day008'),
-    array('7', 'Todo List',         'day007'),
-    array('6', 'View Composer',     'day006'),
-    array('5', 'Login via Google',  'day005_login'),
-    array('4', 'Login',             'day004_login'),
-    array('3', 'Roman Revisited',   'roman2'),
-    array('2', 'Roman Numerals',    'roman'),
-    array('1', 'Homepage',          ''),        
+    //  Day #   Title                 path
+    array('12', 'Image Manipulation', 'day012'),
+    array('11', 'Image Cropping',     'day011'),
+    array('10', 'Playing with CSS',   'day010'),
+    array('9',  'Repository Todo',    'day009'),
+    array('8',  'RESTful Todos',      'day008'),
+    array('7',  'Todo List',          'day007'),
+    array('6',  'View Composer',      'day006'),
+    array('5',  'Login via Google',   'day005_login'),
+    array('4',  'Login',              'day004_login'),
+    array('3',  'Roman Revisited',    'roman2'),
+    array('2',  'Roman Numerals',     'roman'),
+    array('1',  'Homepage',           ''),        
 
 );
