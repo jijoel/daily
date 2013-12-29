7e1c77a03a8048a6738eb517beaba4488a9e9baa
Joel (home) <jijoel@yahoo.com>
2013-12-29 07:42:05 -1000

    add application logic (adapter, controller, view)

---
+++ app/tests/Days/Day022/FactorsControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @group now
+ */
+class FactorsControllerTest extends ControllerTestCase
+{
+    public function testIndex()
+    {
+        $this->call('GET', '/day022');
+        $this->assertResponseOk();
+    }
+
+    public function testStoreFails()
+    {
+        $this->setupValidator(False);
+
+        $this->call('POST', '/day022');
+
+        $this->assertRedirectedToRoute('day022.index');
+        $this->assertHasOldInput();
+        $this->assertSessionHas('errors');
+    }
+
+    public function testStore()
+    {
+        $data = array('number'=>'200');
+
+        $this->call('POST', '/day022', $data);
+
+        $this->assertRedirectedToRoute('day022.index');
+    }
+}
+
