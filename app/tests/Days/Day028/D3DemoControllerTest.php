8718e6e9b9d9f1c87e330ca4d5770a0ed6aeeb89
Joel (work) <joel@kalani.com>
2014-01-21 13:29:23 -1000

    sample project

---
+++ app/tests/Days/Day028/D3DemoControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @group now
+ */
+class D3DemoControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $this->call('GET', '/day028');
+        $this->assertResponseOk();
+    }
+}
+
