b6aebc32872106df2e5a4eff26a30859027142f8
Joel (home) <jijoel@yahoo.com>
2014-02-04 07:33:44 -1000

    update home page to remove note

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -14,8 +14,6 @@
 
         <p>I'm Joel, and I'm very inspired by <a href="http://jenniferdewalt.com/">Jennifer Dewalt's</a> 180 web sites in 180 days project. I don't think I'm quite that ambitious--just a 100 day challenge. :-) This will be a playground for me to spend a little time each day experimenting with web tools and techniques (both client and server side). I'm generally only going to spend about an hour per day on these projects--it's a warm-up and test-bed more than anything, so they'll generally be pretty simple things. In some cases, I'll also revisit them when I come up with a better way to do something. The project source is available on <a href="https://github.com/jijoel/daily">github</a>.</p>
 
-        <p>For at least the next little while, updates will be very slow. My home computer hasn't been able to get onto the Internet, so I've had to try to carve out a little bit of time in the office. That has proven to be trickier than I'd like...</p>
-
         <hr>
 
         <dl>
