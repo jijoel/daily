8727e2abe9023cdd98a9b48be49b1638d515e436
Joel (home) <jijoel@yahoo.com>
2014-04-04 08:07:39 -1000

    use external notification view

--- app/views/layouts/bootstrap3.blade.php
+++ app/views/layouts/bootstrap3.blade.php
@@ -18,22 +18,7 @@
 
         <h1><a href="{{$dayLink}}">{{ 'Day ' . $day . ': ' . $dayTitle }}</a></h1>
 
-        @if (Session::has('message'))
-            <div class="flash alert">
-                <p>{{ Session::get('message') }}</p>
-            </div>
-        @endif
-
-        @if(Session::has('errors'))
-            <div class="flash alert error">
-            <p>There were errors:
-            <ul>
-            @foreach($errors->all('<li>:message</li>') as $error)
-                {{$error}}
-            @endforeach
-            </ul>
-            </div>
-        @endif
+        @include('partials.notifications')
 
         @yield('content')
 
