4629931284fd5305833c69ea3eebc9b626b5a73e
Joel (work) <joel@kalani.com>
2014-04-22 14:48:55 -1000

    update demo forms to use wizard

--- app/views/days/042/form1.blade.php
+++ app/views/days/042/form1.blade.php
@@ -1,6 +1,6 @@
 @include('partials.notifications')
 
-{{ KForm::open(array('url'=>$url))->horizontal() }}
+{{ KForm::open(array('url'=>$url))->horizontal()->wizard('p1') }}
     {{ KForm::text('f1')->label('Field1*') }}
     {{ KForm::text('f2')->label('Field2*') }}
     {{ KForm::text('f3')->label('Field3*') }}
