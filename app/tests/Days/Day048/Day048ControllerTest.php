0f1d2d57219382e2bb8e57a02d9784dff862352f
Joel (home) <jijoel@yahoo.com>
2014-05-02 10:43:45 -1000

    create table before running tests

--- app/tests/Days/Day048/Day048ControllerTest.php
+++ app/tests/Days/Day048/Day048ControllerTest.php
@@ -5,6 +5,8 @@ class Day048ControllerTest extends TestCase
 {
     public function testIndex()
     {
+        (new CreateDay044FilesTable)->up();
+
         $this->call('GET', '/day048');
         $this->assertResponseOk();
     }
--- app/tests/Days/Day049/Day049ControllerTest.php
+++ app/tests/Days/Day049/Day049ControllerTest.php
@@ -5,6 +5,8 @@ class Day049ControllerTest extends TestCase
 {
     public function testIndex()
     {
+        (new CreateDay044FilesTable)->up();
+
         $this->call('GET', '/day049');
         $this->assertResponseOk();
     }
