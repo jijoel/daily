0f81f34855707331ed83aced8c08fcc00e032c55
Joel (work) <joel@kalani.com>
2014-07-12 11:11:36 -1000

    add config and route for new day

--- app/config/days.php
+++ app/config/days.php
@@ -3,6 +3,7 @@
 return array(
 
     //  Day #    path             Title
+    array('52',  'day052',        'Pre-defined Carbon formats'),
     array('51',  'day051',        'Performance tests'),
     array('50',  'day050',        'Angular Image Upload'),
     array('49',  'day049',        'Ajax (jQuery) Image Upload'),
