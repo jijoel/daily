a5aab6e5f625dc182360ec1b49e1881f19ae394a
Joel (home) <jijoel@yahoo.com>
2014-07-20 23:30:53 -1000

    create table to pass test

--- app/tests/Days/Day055/Day055ControllerTest.php
+++ app/tests/Days/Day055/Day055ControllerTest.php
@@ -3,6 +3,13 @@
 
 class Day055ControllerTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        (new CreateDay055BlogTable)->up();
+    }
+
     public function testIndex()
     {
         $this->call('GET', '/day055');
