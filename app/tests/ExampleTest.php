2268093af98fc1092d4fdbf49cd21d7dee9abe8d
Joel (work) <joel@kalani.com>
2014-07-25 00:04:37 -1000

    use DateRange package

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -1,6 +1,7 @@
 <?php
 
-class ExampleTest extends TestCase {
+class ExampleTest extends TestCase 
+{
 
     /**
      * A basic functional test example.
@@ -12,6 +13,36 @@ class ExampleTest extends TestCase {
         $crawler = $this->client->request('GET', '/');
 
         $this->assertTrue($this->client->getResponse()->isOk());
+    }
+
+/**
+ * @group now
+ */
+    public function testDateRange()
+    {
+        $date = KDate::make('now');
+        var_dump($date->start_title);
+
+        $date = KDate::make(Kalani\DateRange\DateRange::NONE);
+        var_dump($date->start);
+
+        $date = KDate::make(KDate::none());
+        var_dump($date->start);
+
+        $date = KDate::make('today');
+        var_dump($date->short);
+
+        $date = KDate::make('today','tomorrow');
+        var_dump($date->short);
+
+        $date = KDate::make('today','next Friday');
+        var_dump($date->short);
+
+        $date = KDate::make('today','last day of this month');
+        var_dump($date->short);
+
+        var_dump($date->short_title);
+        var_dump($date->start_title);
     }
 
 }
